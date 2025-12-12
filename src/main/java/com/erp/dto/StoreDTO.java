ca6c3f3b1da43894f77b8094619b40af9e473df6
moonycode <jmoony0903@gmail.com>
2025-12-12 17:39:11 +0900

    fix : fix StoreDTO toDTO add if null

--- src/main/java/com/erp/dto/StoreDTO.java
+++ src/main/java/com/erp/dto/StoreDTO.java
@@ -30,8 +30,20 @@ public class StoreDTO {
     private String longitude;
     private String storeImage;
     private String menuStopRole;
-
     public static StoreDTO toDTO(Map<String, String> data) {
+
+        String opened = data.get("openedDate");
+        LocalDate openedDate = null;
+        if (opened != null && !opened.isBlank()) {
+            openedDate = LocalDate.parse(opened);
+        }
+
+        String closed = data.get("closedDate");
+        LocalDate closedDate = null;
+        if (closed != null && !closed.isBlank()) {
+            closedDate = LocalDate.parse(closed);
+        }
+
         return StoreDTO.builder()
                 .storeName(data.get("storeName"))
                 .storeStatus(data.get("storeStatus"))
@@ -39,11 +51,13 @@ public class StoreDTO {
                 .longitude(data.get("longitude"))
                 .latitude(data.get("latitude"))
                 .storePhoneNumber(data.get("storePhoneNumber"))
-                .openedDate(LocalDate.parse(data.get("openedDate")))
+                .openedDate(openedDate)
+                .closedDate(closedDate)
                 .openTime(data.get("openTime"))
                 .closeTime(data.get("closeTime"))
                 .storeImage(data.get("image"))
                 .menuStopRole("N")
                 .build();
     }
+
 }
